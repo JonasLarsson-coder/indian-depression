5115762b8ecca7a1d920d2a84c0a31a79660faa6
JonasLarsson-coder <Jonas.larsson.data24sto@edu.tucsweden.se>
2025-03-28 16:40:59 +0100

    created main.js

---
+++ main.js
@@ -0,0 +1,5 @@
+import './libs/liveReload.js';
+import addMdToPage from './libs/addMdToPage.js';
+import dbQuery from "./libs/dbQuery.js";
+import tableFromData from './libs/tableFromData.js'
+import drawGoogleChart from "./libs/drawGoogleChart.js";
